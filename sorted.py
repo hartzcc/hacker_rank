eebbb15d4272226ce35f8a3c7d1a6e87fc91ea39
hartzcc <hartzcc@gmail.com>
2021-02-10 16:35:49 +0000

    2/10

--- sorted.py
+++ sorted.py
@@ -1,3 +1,19 @@
-p = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
-sorted(p, key=lambda x: x[1])
-print(*sorted[p[0][0], p[1][0])
+students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+rank = 1   
+
+scores = []
+for student in students:
+    if student[1] not in scores:
+        scores.append(student[1])
+
+sorted_scores = list(sorted(scores))
+
+rank_score = sorted_scores[rank]
+rank_students = []
+for student in students:
+    if student[1] == rank_score:
+        rank_students.append(student)
+
+sorted_rank_students = sorted(rank_students)
+for student in sorted_rank_students:
+    print(student[0])
