f484ae954d90bb19b082207e93d322deeaf2caeb
hartzcc <hartzcc@gmail.com>
2021-02-26 14:51:48 +0000

    2/26

--- csv_pickle.py
+++ csv_pickle.py
@@ -1,13 +1,14 @@
-import cvs
+import csv
+import pickle
 from os import path
 
 if path.exists('memory.cvs'):
     with open('memory.cvs', 'r') as file:
-        reader = cvs.reader(file)
+        reader = csv.reader(file)
         for row in reader:
             print(row)
 else:
     with open('memory.cvs', 'w') as file:
-        writer = cvs.writer(file)
+        writer = csv.writer(file)
         writer.writerow([1,2,3])
 
